5453d532f8d2c3992328f6281d24d08434fadaf8
Aman Ahuja <amanjiofficial@gmail.com>
2020-07-05 15:50:06 +0530

    Script for Launching and running VM for given  Duration

---
+++ tenjint_script/README.md
@@ -0,0 +1,14 @@
+Arguments List 
+
+1. Tenjint Configuration Path
+2. Unique Domain Name
+3. Unique Snapshot Path
+4. Qemu Emulator Path
+5. Backing File Path
+6. Time To Run
+
+Example 
+
+
+sudo python3 libvirt_first.py /home/dell/tenjint_config.yml tenjint_first_1 /home/dell/Downloads/tpub_2.qcow2 /home/dell/Documents/opensource/tenjint/qemu/x86_64-softmmu/qemu-system-x86_64 /home/dell/Downloads/ub.qcow2 20
+
--- tenjint_script/libvirt_first.py
+++ tenjint_script/libvirt_first.py
@@ -1,8 +1,26 @@
 from __future__ import print_function
 import sys
 import libvirt
+from datetime import datetime
+import uuid
+import subprocess
 
-xmlconfig = "<domain type='kvm' xmlns:qemu='http://libvirt.org/schemas/domain/qemu/1.0'> <name>tenjint_firse</name>  <uuid>c7a5fdbd-edaf-9455-926a-d65c16db1809</uuid>  <memory>5000000</memory> <vcpu>1</vcpu> <os><type arch='x86_64' machine ='pc-i440fx-4.2'>hvm</type></os><clock offset='utc'/>  <on_poweroff>destroy</on_poweroff>  <on_reboot>restart</on_reboot>  <on_crash>destroy</on_crash>  <devices>    <emulator>/home/dell/Documents/opensource/tenjint/qemu/x86_64-softmmu/qemu-system-x86_64</emulator>    <disk type='file' device='disk'>      <source file='/home/dell/Downloads/ub.qcow2'/>      <driver name='qemu' type='qcow2'/>      <target dev='hda'/> </disk> <input type='mouse' bus='ps2'/><graphics type='vnc' port='-1'/></devices> <qemu:commandline><qemu:arg value='-L' /><qemu:arg value='/usr/share/seabios' /></qemu:commandline></domain>"
+
+vmi_config_path = sys.argv[1]
+domain_name = sys.argv[2]
+unique_snapshot_path = sys.argv[3]
+#unique_snapshot_name = sys.argv[7]
+domain_uuid = uuid.uuid4()
+emulator_path = sys.argv[4]
+source_file_qcow2 = sys.argv[5]
+
+process = subprocess.Popen(['qemu-img', 'create', '-f', 'qcow2', '-b', source_file_qcow2, unique_snapshot_path],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+print(stdout)
+
+xmlconfig = "<domain type='kvm' vmi='on' vmi_configs='{}' xmlns:qemu='http://libvirt.org/schemas/domain/qemu/1.0'> <name>{}</name>  <uuid>{}</uuid>  <memory>5000000</memory> <vcpu>1</vcpu> <os><type arch='x86_64' machine ='pc-i440fx-4.2'>hvm</type></os><clock offset='utc'/>  <on_poweroff>destroy</on_poweroff>  <on_reboot>restart</on_reboot>  <on_crash>destroy</on_crash>  <devices><emulator>{}</emulator><disk type='file' device='disk'><driver name='qemu' type='qcow2'/><source file='{}'/><backingStore type='file'><format type='qcow2'/><source file='{}'/></backingStore><target dev='hda'/></disk><input type='mouse' bus='ps2'/><graphics type='vnc' port='-1'/></devices> <qemu:commandline><qemu:arg value='-L' /><qemu:arg value='/usr/share/seabios' /></qemu:commandline></domain>".format(vmi_config_path, domain_name, domain_uuid, emulator_path, unique_snapshot_path, source_file_qcow2)
 
 conn = libvirt.open('qemu:///system')
 if conn == None:
@@ -19,6 +37,14 @@ if dom.create() < 0:
     exit(1)
 
 print('Guest '+dom.name()+' has booted', file=sys.stderr)
-
 conn.close()
-exit(0)
+
+timetorun = float(sys.argv[6])
+starttime = datetime.now()
+starttime = datetime.timestamp(starttime)
+print(starttime)
+while datetime.timestamp(datetime.now()) <= starttime + timetorun:
+    continue
+conn = libvirt.open('qemu:///system')
+conn.lookupByName(domain_name).destroy()
+conn.lookupByName(domain_name).undefine()
